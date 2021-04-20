ba0a83592573892f92a43f721fdef8347c62138e
Minjae Jeong <mijeong@c4r1s6.42seoul.kr>
2021-04-20 17:41:16 +0900

    ex07

--- cpp01/ex07/ex07.cpp
+++ cpp01/ex07/ex07.cpp
@@ -10,9 +10,30 @@ int main(int ac, char **av)
 		std::cout << "입력 갯수 틀림!!\n";
 		return 1;
 	}
-	std::string s1(av[1]);
-	std::string s2(av[2]);
-	fin.open(av[0])
-	if (fin.is_open)
+	std::string	str;
+	std::string	s1(av[2]);
+	int			i;
+	fin.open(av[1]);
+	fout.open("FILENAME.replace");
+	if (fin.is_open())
+	{
+		while (!fin.eof())
+		{
+			std::getline(fin, str);
+			i = 0;
+			// 찾고
+			while ((i = (int)str.find(s1, i)) != -1)
+			{
+				// 바꾸고
+				str.replace(i, s1.length(), av[3]);
+				i++;
+			}
+			fout << str;
+			if (!fin.eof())
+				fout << std::endl;
+		}
+		fin.close();
+		fout.close();
+	}
 	return 0;
 }
